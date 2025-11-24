e8858e9901a559b7123fa45cbdbf62831bdd2855
lalitharw <lalitharwate90@gmail.com>
2025-11-24 10:50:56 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const Sequelize = require("sequelize")
+const bodyParser = require('body-parser');
+const { body, validationResult } = require("express-validator")
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const PORT = 9000
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: "mysql"
+});
+
+app.post("/save-data",
+    [
+        body("name").notEmpty(),
+        body("email").isEmail(),
+        body("password").notEmpty()
+    
+], async(req,res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    res.status(200).json({
+        success:"Successful Sign Up!",
+        data: req.body
+    })
+})
+
+app.get("/",async(req,res) => {
+    res.send("Hello asd")
+    try{
+        await sequelize.authenticate()
+        console.log("Connection has been established successfully")
+    }
+    catch(error){
+        console.log("Unable to connect:" , error)
+    }
+})
+
+app.listen(PORT,(error) => {
+    console.log("Hello world again34")
+})
---
+++ readme.md
@@ -0,0 +1,10 @@
+## To Validate Input in Express.js
+https://www.digitalocean.com/community/tutorials/how-to-handle-form-inputs-efficiently-with-express-validator-in-express-js
+
+## To Take POST request always use body-parser and in POSTMAN use raw json
+
+## Question Regarding Docker not reflecting code changes
+Use  CHOKIDAR_USEPOLLING: "true" CHOKIDAR_INTERVAL: "100" it will check for code changes
+http://stackoverflow.com/questions/44643045/running-development-server-with-create-react-app-inside-of-a-docker-container
+##
+Can lead to high cpu usage
